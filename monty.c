61f3b33cc9bb6abb154cc2b6ccf384e563cca03f
Janengethe <janengethew@gmail.com>
2021-05-13 12:47:07 +0000

    compressed for file opening

--- monty.c
+++ monty.c
@@ -8,8 +8,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd;
 	char *buffer;
+	FILE *file;
 	ssize_t n_read;
 
 	if (argc != 2)
@@ -17,8 +17,8 @@ int main(int argc, char *argv[])
 		printf("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
+	file = fopen(argv[1], "r");
+	if (file == NULL)
 	{
 		printf("Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
